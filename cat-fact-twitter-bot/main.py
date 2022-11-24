a85a96a3549b76d6d562300f92b2c6708ca6c36a
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-11-24 12:19:09 -0500

    commit

--- cat-fact-twitter-bot/main.py
+++ cat-fact-twitter-bot/main.py
@@ -9,3 +9,7 @@ import redis
 from requests.auth import AuthBase, HTTPBasicAuth
 from requests_oauthlib import OAuth2Session, TokenUpdated
 from flask import Flask, request, redirect, session, url_for, render_template
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+r = redis.from_url(os.environ["REDIS_URL_DOGS"])
